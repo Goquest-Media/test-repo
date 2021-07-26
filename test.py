dab66a866e753bd3a5b03c43fb730b07b5667c36
vrushabmulik <vrushab.mulik@goquestmedia.com>
2021-07-26 12:27:12 +0530

    added funtion to develop branch

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 import time
-for _ in range(5):
-    print(datetime.now())
-    time.sleep(2)
+def get_time_loop(): 
+    for _ in range(5):
+        print(datetime.now())
+        time.sleep(2)
 #time delay set to 2
+
+def get_double(num):
+    return num*2
+
+print(get_double(4))
